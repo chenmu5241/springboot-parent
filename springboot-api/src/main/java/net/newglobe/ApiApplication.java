397191f0effa72328ab0548baabc1967327e33ae
chenmu5241 <chenmu5241@163.com>
2019-12-10 12:02:32 +0800

    调整代码

--- springboot-api/src/main/java/net/newglobe/ApiApplication.java
+++ springboot-api/src/main/java/net/newglobe/ApiApplication.java
@@ -1,11 +1,13 @@
 package net.newglobe;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+//@EnableAutoConfiguration
 public class ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
